91ecf37ccdb3b88d614402bdaf7197ed2ca5a874
Daniel <drutledge@pros.com>
2019-05-18 22:36:34 -0500

    Add react-ga package for google analytics

--- js/components/category.js
+++ js/components/category.js
@@ -1,5 +1,6 @@
 import { buildURL } from '../utils'
 import { h } from 'preact'
+import ReactGA from 'react-ga';
 
 const food = (
   <svg viewBox="0 0 480 480.00004" width="480pt" xmlns="http://www.w3.org/2000/svg">
@@ -76,6 +77,10 @@ const icons = { food, family, health, housing, education, legal, employment, mon
 const Category = ({ name, description }) => (
   <div className="col-lg-3 col-md-3 col-sm-6 col-6 py-3">
     <a className="card category"
+      onClick={ReactGA.event({
+        category: 'Category Click',
+        action: name
+      })}
       href={`${buildURL('/search.html')}?service-checks=HILSC+Verified&service-type=${name.toLowerCase()}${(process.env.IS_DISASTER_MODE === 'true' && '&disaster-only=true') || ''}`}>
       <div className="card-body text-center">
         <h5>{name}</h5>
