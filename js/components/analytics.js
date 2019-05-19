1d0bf770ede320386df3b217a2d04bfe71632c81
Robyn Moscowitz <robyn.moscowitz@gmail.com>
2019-05-18 23:50:17 -0500

    sample ga embedded chart

--- js/components/analytics.js
+++ js/components/analytics.js
@@ -3,6 +3,40 @@ import Layout from './layout'
 
 import { buildURL } from '../utils'
 
+gapi.analytics.ready(function() {
+
+  /**
+   * Authorize the user with an access token obtained server side.
+   */
+  gapi.analytics.auth.authorize({
+    'serverAuth': {
+      'access_token': process.env.GA_ACCESS_TOKEN
+    }
+  });
+
+  /**
+   * Creates a new DataChart instance showing sessions over the past 30 days.
+   * It will be rendered inside an element with the id "chart-1-container".
+   */
+  var dataChart1 = new gapi.analytics.googleCharts.DataChart({
+    query: {
+      'ids': 'ga:78880644',
+      'start-date': '30daysAgo',
+      'end-date': 'yesterday',
+      'metrics': 'ga:sessions,ga:users',
+      'dimensions': 'ga:date'
+    },
+    chart: {
+      'container': 'chart-1-container',
+      'type': 'LINE',
+      'options': {
+        'width': '100%'
+      }
+    }
+  });
+  dataChart1.execute();
+});
+
 export default () => {
   return (
     <Layout crumbs={[
@@ -14,6 +48,9 @@ export default () => {
       }
     ]}>
       <div className="container" id="analytics-container">
+        <div className="row">
+          <div id="chart-1-container"></div>
+        </div>
         <div className="row">
           <div className="col">
             <label>Interesting Chart #1</label>
